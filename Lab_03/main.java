e6ff421afa031d35414323c2babb19e7ca16fd23
ItaloCA <32741970+ItaloCA@users.noreply.github.com>
2018-03-28 14:53:07 -0300

    nomes

--- Lab_03/main.java
+++ Lab_03/main.java
@@ -1,7 +1,20 @@
+/*
+Grupo:  Italo Cavalcante de Abreu - Classes concretas
+	João Vitor Pinheiro Nascimento - classe Colorida e questao 5
+	Jordan Ferreira - Classe abstrata Figura e chefia
+//*/
+
 class Main{
 	public static void main(String argvs[]){
+		
+		Quadrado quad1 = new Quadrado(1f);
+		Quadrado quad2 = new Quadrado(2f, 4f);
+		Circulo circ1 = new Circulo();
+		Circulo circ2 = new Circulo(2f,5f,5f);
+	
+		
 		Figura[] f = new Figura[10];
-		boolean check;
+
 		for(int i = 0; i<10; i++){
 			if(i%2 == 0){
 				f[i]= new Quadrado(5f,5f,4f);
